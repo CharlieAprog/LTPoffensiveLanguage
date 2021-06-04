eb32d2780c70e6a51a908d4d19b672b5dc124f14
CharlieAprog <charlie.albietz.uni@gmail.com>
2021-06-04 11:01:43 +0200

    quick error fix

--- models/LSTMmain.py
+++ models/LSTMmain.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 dataset = pd.read_csv('data/training.tsv', sep='\t')
-training_tweeets = self.dataset[['tweet']].to_numpy()
-training_labels = self.dataset[['subtask_a']].to_numpy()
+training_tweeets = dataset[['tweet']].to_numpy()
+training_labels = dataset[['subtask_a']].to_numpy()
 #TODO set up validation set
 # set up testing set
 # set up 
